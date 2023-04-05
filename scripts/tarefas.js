ba656b4cfc4e462614e1831548201f2d56bae84d
Samuel Ramos <104317445+Thesamramos@users.noreply.github.com>
2023-04-05 17:53:12 -0400

    função de voltar a tarefa feita

--- scripts/tarefas.js
+++ scripts/tarefas.js
@@ -204,6 +204,9 @@ async function concluirTarefa(id) {
 
         tarefaTerminada.innerHTML = ` 
         <li class="tarefa" id="task-${data.id}">
+        <button class="seta" onclick="voltarTarefa('${data.id}')">
+        <img src="./assets/seta-para-cima.png" alt="seta">
+        </button>
         <div class="descricao">
         <p class="nome">${data.description}</p>
         <p class="timestamp">Concluida em: ${dataTarefaTerminada.toLocaleDateString()}</p>
@@ -215,6 +218,49 @@ async function concluirTarefa(id) {
     `
         tarefasTerminadas.appendChild(tarefaTerminada);
         localStorage.setItem(`concluida-${id}`, JSON.stringify(data));
+    }
+
+
+};
+
+async function voltarTarefa(id) {
+
+    let tarefaTerminada = document.getElementById(`task-${id}`);
+
+    let settings = {
+        method: 'PUT',
+        headers: {
+            "content-type": "application/json",
+            "authorization": localStorage.getItem('token')
+        },
+        body: JSON.stringify({ "completed": false })
+    }
+    const resposta = await fetch(`https://todo-api.ctd.academy/v1/tasks/${id}`, settings);
+    let data = await resposta.json();
+    console.log(data);
+
+    tarefaTerminada.remove();
+    localStorage.removeItem(`concluida-${id}`);
+    if (resposta.ok) {
+
+        let divTarefas = document.getElementById('tarefas');
+        let tarefa = document.createElement('li');
+        let dataTarefa = new Date();
+
+        tarefa.innerHTML = `
+            <li class="tarefa" id="task-${data.id}">
+            <div class="not-done" onclick="concluirTarefa('${data.id}')"></div>
+            <div class="descricao">
+            <p class="nome">${data.description}</p>
+            <p class="timestamp">Criada em: ${dataTarefa.toLocaleDateString()}</p>
+            </div>
+            <button class="trash" onclick="ativarLixeira('${data.id}')">
+            <img src="./assets/excluir.png" alt="lixeira">
+            </button>
+            </li>
+        `
+        divTarefas.appendChild(tarefa);
+        localStorage.setItem(`tarefa-${data.id}`, JSON.stringify(data));
     }
 
 
